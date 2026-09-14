293f3950aae91bb7621c73460ab8b52017db2704
Iram Remigio Alvarez Rocha <iram.alvarezr@uanl.mx>
2026-09-14 15:26:50 -0600

    feat: add DispositivoEntrada, Raton, and Teclado classes with string representation

---
+++ poo/mundo_pc/raton.py
@@ -0,0 +1,30 @@
+from dispositivo_entrada import DispositivoEntrada
+
+
+class Raton(DispositivoEntrada):
+    
+    contador_ratones = 0
+    
+    def __init__(self, marca, tipo_entrada):
+        Raton.contador_ratones += 1
+        self.id_raton = Raton.contador_ratones
+        # self.marca = marca
+        # self.tipo_entrada = tipo_entrada
+        super().__init__(marca, tipo_entrada)
+        
+    def __str__(self):
+        return(f'''
+              Id = { self.id_raton }
+              Marca = { self.marca }
+              Tipo Entrada = { self.tipo_entrada }
+              Total de ratones = {self.contador_ratones}
+              ''')
+        
+# Código principal
+
+if __name__ == "__main__":
+    raton = Raton('HP', 'USB Tipo C')
+    print(raton)
+    
+    raton2 = Raton('HP', 'USB')
+    print(raton2)
