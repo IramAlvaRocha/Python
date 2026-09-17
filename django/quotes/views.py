b397f26f147f107235fac81c4095a0c3f23f1933
Iram Remigio Alvarez Rocha <iram.alvarezr@uanl.mx>
2026-09-17 17:11:25 -0600

    feat: refactor quote retrieval logic in views to use a dictionary for improved readability and maintainability

--- django/quotes/views.py
+++ django/quotes/views.py
@@ -1,38 +1,30 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 from django.shortcuts import render
 
 # Create your views here.
 
+days_of_week = {
+    "monday": 'Pienso... luego existo.',
+    "tuesday": "La vida es una lenteja.",
+    "wednesday": "Quiero ser minero.",
+    "thursday": "Hay una vida en el más allá.",
+    "friday": "Estar roto tambien es ser libre.",
+    "saturday": "Vive como si fuera el último día.",
+    "sunday": "Da un poquito más cada día."
+}
+
 # Funcion que recibe el valor que cae en la sentencia de control que redirecciona a la vista correcta
 def days_week(request, day):
-    quote_text = None
-    if day == 'monday': 
-        quote_text = 'Pienso... luego existo.'
-    elif day == "tuesday":
-        quote_text = "La vida es una lenteja"
-    elif day == "wensday":
-        quote_text = "Quiero ser minero"
-    elif day == "thursday":
-        quote_text = "Hay una vida en el más allá"
-    elif day == "friday":
-        quote_text = "Estar roto tambien es ser libre"
-    else:
-        return HttpResponseNotFound("Día no válido")  
-    return HttpResponse(quote_text)
-
+    try:
+        quote_text = days_of_week[day] 
+        return HttpResponse(quote_text)
+    except:
+        return HttpResponseNotFound("No hay frase para este día")
+    
 def days_week_with_number(request, day):
-    quote_text = None
-    if day == 1: 
-        quote_text = 'Pienso... luego existo.'
-    elif day == 2:
-        quote_text = "La vida es una lenteja"
-    elif day == 3:
-        quote_text = "Quiero ser minero"
-    elif day == 4:
-        quote_text = "Hay una vida en el más allá"
-    elif day == 5:
-        quote_text = "Estar roto tambien es ser libre"
-    else:
-        return HttpResponseNotFound("Día no válido")  
-    return HttpResponse(quote_text)
+   days = list(days_of_week.keys())
+   if day > len(days):
+       return HttpResponseNotFound("El día no existe")
+   redirect_day = days[day-1]
+   return HttpResponseRedirect(f"/quotes/{redirect_day}")
